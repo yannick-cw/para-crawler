8bd5fa61233c40a3bff5322c8b57f9a50025485f
437580 <yannick.gladow@holidaycheck.com>
2016-09-06 08:17:49 +0200

    added user

--- src/main/scala/ParaParser.scala
+++ src/main/scala/ParaParser.scala
@@ -6,37 +6,41 @@ import scala.concurrent.duration._
   */
 
 case class User(email: String, lookingFor: List[String])
+
 case class ParseResult(href: String, id: Int, title: String, imgSrc: String)
+
 case class Result(user: User, parseResults: List[ParseResult])
 
 object ParaParser extends App with Parsing {
 
   val system = ActorSystem()
-  val users = List(User("yannick.gladow@gmail.com", List(
-    "woody", "wood", "valley", "wani", "bright", "gradient"
-  )))
+  val users = List(User("yannick.gladow@gmail.com", List("woody", "wood", "valley", "wani", "bright", "denali")),
+    User("antonia.kahlert@gmail.com", List("woody", "wood", "valley", "wani", "prion", "koyote", "koyot")))
   var currentId = 46445
 
   import system.dispatcher
+
   system.scheduler.schedule(1 second, 1 hour) {
     val (results, id) = giveMeNewResults(users)(currentId)
     results.foreach { res =>
 
       val msg =
         s"""
-          |Hallo ${res.user.email}<br>
-          |<br>
-          |Es scheint wieder neue Angebote für deine Suche nach: ${res.user.lookingFor.mkString(", ")}<br>
-          |zu geben. <br>
-          |<br>
-          |Check doch mal diese neuen Angebote<br>
-          |<br>
-          |${res.parseResults.map( result =>
+           |Hallo ${res.user.email}<br>
+           |<br>
+           |Es scheint wieder neue Angebote für deine Suche nach: ${res.user.lookingFor.mkString(", ")}<br>
+           |zu geben. <br>
+           |<br>
+           |Check doch mal diese neuen Angebote<br>
+           |<br>
+           |${
+          res.parseResults.map(result =>
             s"""Titel: ${result.title}<br>
-               |Link: ${result.href}<br>
-               |<br>
-               |<img src="${result.imgSrc}" alt="img"/><br>
-             """.stripMargin).mkString("<br> <br>")}
+                |Link: ${result.href}<br>
+                |<br>
+                |<img src="${result.imgSrc}" alt="img"/><br>
+             """.stripMargin).mkString("<br> <br>")
+        }
         """.stripMargin
       sendMail(msg, res.user.email)
     }
